2e0f946363d695d86216b986ce3da1b9c0447fc8
Andrés Cidel <ac@manoderecha.mx>
2013-10-21 10:04:14 -0500

    Added ajax

--- js/main.js
+++ js/main.js
@@ -31,43 +31,45 @@
 				var img = new Image();
 				img.src = ruteImg;
 				//img.setAttribute("class", "calavera");
-				$(img).css("background", currentBackground);
 				img.onload = function(){
 					$("#buttons").find(".calavera img").first().remove();
 					$("#buttons").find(".calavera").append(img);
 					wait.removeClass("show");
 				}
 			},
-			form = $("form[name=send]");
+			form = $("form[name=send]"),
+			textName = $(".text");
 
 		buttons.eq(0).click(function(a){
 			a.preventDefault();
 			currentURL = (currentURL - 1 < 0) ? lastURL : currentURL - 1;
 			wait.addClass("show");
 			newImage(rute + url[currentURL] + cColors[currentColor]);
-			console.log(rute + url[currentURL] + cColors[currentColor]);
+			textName.attr("class", "text " + url[currentURL].substr(0, url[currentURL].length - 1) + " " + cColors[currentColor].substr(0, cColors[currentColor].length - 4));
+			//console.log(rute + url[currentURL] + cColors[currentColor]);
 		});
 		buttons.eq(1).click(function(a){
 			a.preventDefault();
 			currentURL = (currentURL + 1 > lastURL) ? 0 : currentURL + 1;
 			wait.addClass("show");
 			newImage(rute + url[currentURL] + cColors[currentColor]);
-			console.log(rute + url[currentURL] + cColors[currentColor]);
+			textName.attr("class", "text " + url[currentURL].substr(0, url[currentURL].length - 1) + " " + cColors[currentColor].substr(0, cColors[currentColor].length - 4));
+			//console.log(rute + url[currentURL] + cColors[currentColor]);
 		});
 		colors.each(function(a){
 			$(this).click(function(b){
 				b.preventDefault();
 				if(currentColor !== a){
 					currentColor = a;
-
 					newImage(rute + url[currentURL]+ cColors[currentColor]);
+					textName.attr("class", "text " + url[currentURL].substr(0, url[currentURL].length - 1) + " " + cColors[currentColor].substr(0, cColors[currentColor].length - 4));
 				}
 			});
 		});
 		backs.each(function(a){
 			$(this).click(function(b){
 				b.preventDefault();
-				var col = currentBackground = $(this).css("background");
+				var col = currentBackground = $(this).data("cl");
 				$(".calavera").css("background", col);
 			});
 		});
@@ -76,6 +78,23 @@
 			.keyup(function(a){
 				var val = $(this).val();
 				if(val.length > 6) $(this).val(val.substr(0,6));
+				else{
+					var steps,
+						spans = [];
+					if(val.length > 0)
+						steps = 3;
+					if(val.length > 2)
+						steps = 2;
+					if(val.length > 4)
+						steps = 1;
+					for(var x = steps, y = 0; y < val.length; x++, y++){
+						var span = $("<span class='ch" + x + "'>");
+						span.html(val[y]);
+						spans.push(span);
+					};
+					textName.html("");
+					textName.append(spans);
+				}
 		});
 
 		form.submit(function(a){
@@ -86,17 +105,23 @@
 					.val();
 			$.ajax({
 				type: "POST",
-				url: "http://162.243.143.152:3000",
+				url: "http://localhost:3000/calas",
 				data:{
 					"cc": currentColor,
 					"cu": currentURL,
-					"tx": text
+					"tx": text,
+					"bk": currentBackground
 				},
 				error: function(a,b){
-					console.log("error");
 				},
 				success: function(a){
-					console.log(a);
+					
+					var img = new Image();
+					img.src = "http://localhost:3000/" + a.url.substr(7, a.url.length - 7);
+					alert(a.url.substr(7, a.url.length - 7));
+					img.onload = function(){
+						$("#result").append(img);
+					}
 				}
 			});
 		});
